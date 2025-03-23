8134a6730e52444da7965b715628cd0512949a20
Manohar-1305 <maddy98989@gmail.com>
2025-03-23 20:11:21 +0530

    Update proxy-app.py

--- proxy-app.py
+++ proxy-app.py
@@ -1,3 +1,6 @@
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
+
+app.config['SESSION_COOKIE_SECURE'] = True  # Only if ALB is using HTTPS
+app.config['SESSION_COOKIE_SAMESITE'] = 'None'
